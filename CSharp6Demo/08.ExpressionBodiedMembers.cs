d6ba82da387ac109cb56c3199e62e4b035dba084
Robert MacLean <robert@sadev.co.za>
2015-03-12 11:15:44 +0200

    Reordered the demos Also split out string interpolation& expresion bodied members

---
+++ CSharp6Demo/08.ExpressionBodiedMembers.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApplication1
+{
+    internal class ExpressionBodiedMembers
+    {
+        public double CostPrice { get; set; }
+
+        private double Tax()
+        {
+            // amazing DB code
+            return 1.14;
+        }
+
+        public double Price
+        {
+            get
+            {
+                return CostPrice * Tax();
+            }
+        }
+    }
+}
---
+++ CSharp6Demo/09.StringInterpolation.cs
@@ -0,0 +1,38 @@
+﻿namespace ConsoleApplication1
+{
+    using System.Linq;
+
+    internal class StringInterpolation
+    {
+        public int Age { get; set; }
+
+        public string Name { get; set; }
+
+        public string[] AnimalsOrderedByFavourite()
+        {
+            //pretend this is in a DB
+            return new[] {
+                "Cat",
+                "Dog",
+                "Goat",
+                "Horse"
+            };
+        }
+
+        public string Info
+        {
+            get
+            {
+                return string.Format("{0} is {1} years old and their favourite animal is {2}", Name, Age, AnimalsOrderedByFavourite().First());
+            }
+        }
+
+        public string DisplayPrice
+        {
+            get
+            {
+                return string.Format("Hamsters cost {0:0.00}", 14.22);
+            }
+        }
+    }
+}

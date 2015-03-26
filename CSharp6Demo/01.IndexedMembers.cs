c88ae87db64e8ce371f234b8822ec5d549df3b93
Robert MacLean <robert@sadev.co.za>
2015-03-26 20:56:15 +0200

    Rearranged the methods for index members demo so it demos a little smoother

--- CSharp6Demo/01.IndexedMembers.cs
+++ CSharp6Demo/01.IndexedMembers.cs
@@ -5,6 +5,19 @@ namespace ConsoleApplication1
 {
     internal static class IndexedMembers
     {
+        private static void CTWO()
+        {
+            var config = new Dictionary<int, string>();
+            config.Add(1, "hi");
+            config.Add(2, "how are you");
+            config.Add(3, "i am fine");
+            config.Add(4, "good bye");
+            foreach (var item in config)
+            {
+                Console.WriteLine("{0} = {1}", item.Key, item.Value);
+            }
+        }
+
         private static void CTHREE()
         {
             var config = new Dictionary<int, string>
@@ -19,19 +32,6 @@ namespace ConsoleApplication1
             {
                 Console.WriteLine("{0} = {1}", item.Key, item.Value);
             }
-        }
-
-        private static void CTWO()
-        {
-            var config = new Dictionary<int, string>();
-            config.Add(1, "hi");
-            config.Add(2, "how are you");
-            config.Add(3, "i am fine");
-            config.Add(4, "good bye");
-            foreach (var item in config)
-            {
-                Console.WriteLine("{0} = {1}", item.Key, item.Value);
-            }
-        }
+        }        
     }
 }
